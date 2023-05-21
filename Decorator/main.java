03ef3005838979a5ca99aa1f005110c7f3ce9537
Karlzzon <oskar000202@gmail.com>
2023-05-21 15:55:08 +0200

    changed main to Main lol

--- Decorator/main.java
+++ Decorator/main.java
@@ -3,7 +3,7 @@ package Decorator;
 import Decorator.Beverages.*;
 import Decorator.Decorators.*;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Beverage beverage = new Latte();
         System.out.println(beverage.getDescription() + " $" + beverage.getCost());
